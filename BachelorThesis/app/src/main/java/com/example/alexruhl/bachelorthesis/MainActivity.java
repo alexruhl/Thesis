bad2be1117bc3905523d01d228dc15658f7987a4
Alexander Ruhl <ruhl@in.tum.de>
2019-12-29 23:22:41 +0100

    Check Internet Connection for Weather

--- BachelorThesis/app/src/main/java/com/example/alexruhl/bachelorthesis/MainActivity.java
+++ BachelorThesis/app/src/main/java/com/example/alexruhl/bachelorthesis/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.alexruhl.bachelorthesis;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,8 +20,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToMorningPage(View view) {
-        Intent intent = new Intent(this, MorningActivity.class);
-        startActivity(intent);
+        if (isOnline()) {
+            Intent intent = new Intent(this, MorningActivity.class);
+            startActivity(intent);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            final Context context = this;
+
+            builder.setTitle("Bitte mit dem Internet verbinden");
+            // builder.setMessage("Möchten Sie bestätigen? ");
+
+            View.OnClickListener dialog;
+            builder.setNegativeButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //Do nothing
+                    // dialog.dismiss();
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+        }
     }
 
     public void goToFoodPage(View view) {
@@ -37,5 +63,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         // do nothing
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+        return (networkInfo != null && networkInfo.isConnected());
     }
 }
